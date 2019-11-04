c44be0cdd118d92455b9c83cedfcd31035126a68
Arthur Fücher <arthurfucher@gmail.com>
2019-11-04 20:06:01 -0300

    remove icone

--- gatsby-config.js
+++ gatsby-config.js
@@ -17,7 +17,6 @@ module.exports = {
         background_color: `#011830`,
         theme_color: `#011830`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality

91d5f97f8f84af960078d3e968357244a57dc51e
Arthur Fücher <arthurfucher@gmail.com>
2019-11-06 20:14:36 -0300

    adiciona horário aos cursos. extrai componente de cursos.

--- src/components/card.js
+++ src/components/card.js
@@ -1,11 +1,12 @@
 import React from "react"
 import styles from "./card.module.css" 
 
-const Card = ({title, date, teacher}) => {
+const Card = ({title, date, time, teacher}) => {
     return <div className={styles.card}>
         <div className={styles.main}>
             <h3>{title}</h3>
             <p>Data de início: {date}</p>
+            <p>Horário: {time}</p>
             <p>Professor(a): {teacher}</p>
         </div>
         <div className={styles.cardBottom}>
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,20 +3,12 @@ import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
-import Card from "../components/card"
+import Cursos from "../components/cursos"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <div style={{
-      display: "flex",
-      justifyContent: "space-between",
-      flexWrap: "wrap"
-    }}>
-      <Card title="Interpretação I" date="01/01/2020" teacher="Leonardo Lemos"/>
-      <Card title="Interpretação II" date="01/01/2020" teacher="Elmer Baumgratz"/>
-      <Card title="Interpretação II" date="01/01/2020" teacher="Nina Alves"/>
-    </div>
+    <Cursos />
   </Layout>
 )
 

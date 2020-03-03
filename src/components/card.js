e4d05a4eb01b7b9eaa30027848072cd580d16246
Arthur Fucher <arthur.fucher@nubank.com.br>
2020-03-02 21:29:50 -0300

    altera card de cursos para seguir estilo novo

--- src/components/card.js
+++ src/components/card.js
@@ -4,14 +4,14 @@ import { navigate } from "gatsby"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 
 const Card = ({title, date, time, teacher}) => {
-    return <div className={styles.card}>
+    return <div className={styles.card} onClick={() => navigate("/curso")}>
         <div className={styles.main}>
             <h3>{title}</h3>
             <p><FontAwesomeIcon icon={["fa","calendar-alt"]} fixedWidth={true}></FontAwesomeIcon> {date}</p>
             <p><FontAwesomeIcon icon={["fa","clock"]} fixedWidth={true}></FontAwesomeIcon> {time}</p>
             <p><FontAwesomeIcon icon={["fa","chalkboard-teacher"]} fixedWidth={true}></FontAwesomeIcon> {teacher}</p>
         </div>
-        <div className={styles.cardBottom} onClick={() => navigate("/curso")}>
+        <div className={styles.cardBottom}>
             Mais informações
         </div>
         
